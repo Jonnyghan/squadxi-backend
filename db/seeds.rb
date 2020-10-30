8b74d07ed261cad7b9af77a37c7750b1c21a928e
Jonathan <Jonnyghan@gmail.com>
2020-10-30 12:48:43 -0400

    set up Team and players model, seeds. namespaced routes as well.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+team = Team.create(name:"Mineola Portuguese", league: "Long Island Soccer Football League")
+
+a = Player.create(name: "Jonathan Ghandforoush", age: 26, position: "St", team_id: team.id)
+b= Player.create(name: "Alex Marano", age: 27, position: "Cb", team_id: team.id)
+c= Player.create(name: "Andrew Ghandforoush", age: 24, position: "Fb", team_id: team.id)
+d= Player.create(name: "Gary Marano", age: 22, position: "Wm", team_id: team.id)
+e= Player.create(name: "Kunal Suri", age: 21, position: "Gk", team_id: team.id)
+f = Player.create(name: "Pete Antoniades", age: 41, position: "St", team_id: team.id)
+g = Player.create(name: "Dom Mauro", age: 27, position: "Cb", team_id: team.id)
+h = Player.create(name: "Jake Raher", age: 22, position: "Fb", team_id: team.id)
+i = Player.create(name: "Mikey Vultaggio", age: 21, position: "Cm", team_id: team.id)
+j = Player.create(name: "Omar Ezat", age: 21, position: "Cm", team_id: team.id)
+k = Player.create(name: "Kareem Harris", age: 21, position: "Wm", team_id: team.id)
